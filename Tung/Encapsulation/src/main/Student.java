4abec0d4cafe6df652d4ae518e320a60682c3816
tuanpv <tuanpv@runsystem.net>
2020-08-15 20:36:27 +0700

    update

---
+++ Tung/Encapsulation/src/main/Student.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Student {
+	// ten, tuoi, mau sac, kich thuoc
+	private String ten;
+	private int tuoi;
+	private int money = 100000;
+	
+	public String getTen() {
+		return ten;
+	}
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+	public int getTuoi() {
+		return tuoi;
+	}
+	public void setTuoi(int tuoi) {
+		this.tuoi = tuoi;
+	}
+	
+	public String getCustomMoney() {
+		// abcx
+		return "100,000 d";
+	}
+}
